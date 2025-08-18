dbd4ca71cd4d9f27ce86498f87beb16ac574764c
paaks100 <pkkodua@gmail.com>
2025-08-18 20:17:31 +0000

    refact: simplify time calculations

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
 import { CACHE_KEY_GENRES } from "../constants";
 import APIClient from "../services/api-client";
+import ms from "ms";
 
 const apiClient = new APIClient<Genre>('/genres');
 
@@ -16,7 +17,7 @@ export interface Genre {
 const useGenres = () => useQuery({
     queryKey: CACHE_KEY_GENRES,
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, // 24h
+    staleTime: ms('24h'),
     initialData: genres
 });
 
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { CACHE_KEY_PLATFORMS } from "../constants";
 import platforms from "../data/platforms";
 import APIClient from "../services/api-client";
+import ms from "ms";
 
 const apiClient = new APIClient<Platform>('/platforms/lists/parents');
 
@@ -14,7 +15,7 @@ export interface Platform {
 const usePlatforms = () => useQuery({
     queryKey: CACHE_KEY_PLATFORMS,
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, // 24h
+    staleTime: ms('24h'),
     initialData: platforms
 });
 
