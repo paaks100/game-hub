1df2f70da3cd58c46b99569acb2a4ad6f059636f
paaks100 <pkkodua@gmail.com>
2025-08-18 17:16:26 +0000

    refact: fetch games with react query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,6 @@
+import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import useData from "./useData";
+import apiClient, { FetchResponse } from "../services/api-client";
 
 export interface Platform {
     id: number;
@@ -17,18 +18,19 @@ export interface Game {
     rating: number;
 }
 
-const useGames = (gameQuery: GameQuery) => (
-    useData<Game>('/games',
-        {
+const useGames = (gameQuery: GameQuery) => useQuery<FetchResponse<Game>, Error>({
+    queryKey: ['games', gameQuery],
+    queryFn: () => apiClient
+        .get<FetchResponse<Game>>('/games', {
             params: {
                 genres: gameQuery.genre?.id,
-                platforms: gameQuery.platform?.id,
+                parent_platforms: gameQuery.platform?.id,
                 ordering: gameQuery.sortOrder,
                 search: gameQuery.searchText
             }
-        },
-        [gameQuery]
-    )
-);
+        })
+        .then(res => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24h
+})
 
 export default useGames;
