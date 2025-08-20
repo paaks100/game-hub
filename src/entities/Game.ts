6170b103fcdf2d6516fabffb7e6bba3595f60e8e
paaks100 <pkkodua@gmail.com>
2025-08-20 19:22:24 +0000

    build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,12 +1,16 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 export interface Game {
     id: number;
     name: string;
     description_raw: string;
     slug: string;
+    genres: Genre[];
     background_image: string;
     parent_platforms: { platform: Platform; }[];
+    publishers: Publisher[];
     metacritic: number;
     rating_top: number;
     rating: number;
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -2,10 +2,13 @@ import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame"
 import { Heading, Spinner } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
+import GameAttributes from "../components/GameAttributes";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
   const { data: game, isLoading, error } = useGame(slug!);
+  console.log(game);
+
 
   if (isLoading) return <Spinner />
 
@@ -15,6 +18,7 @@ const GameDetailPage = () => {
     <>
       <Heading size='4xl'>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game} />
     </>
   )
 }
